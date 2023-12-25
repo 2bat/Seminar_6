448dece6fcfe1df28e37de0071bd1f69f95b4e7c
Vlad <bodroon@gmail.com>
2023-12-25 18:44:03 +0300

    залил папки и гитигнор

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+public class Task2
+{
+    public static void Main(string[] args)
+    {
+        string input = "hElLo_WoRd!";
+        string result = input.ToLower();
+        Console.WriteLine(result);
+    }
+}
